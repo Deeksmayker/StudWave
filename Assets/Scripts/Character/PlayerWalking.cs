f4887a2e21bc4536de4836a2487ff37d64a32005
Dmitry Tiulenev <lokkins334@gmail.com>
2021-04-11 23:19:42 +0500

    Написал небольшую систему передвижения по нажатию. Еще не расставил объекты, по которым нельзя будет ходить.

---
+++ Assets/Scripts/Character/PlayerWalking.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerWalking : MonoBehaviour
+{
+    public LayerMask ClickableLayers;
+
+    private NavMeshAgent agent;
+
+    [SerializeField] private new Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = 30f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+        {
+            Ray ray = camera.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, 1000, ClickableLayers))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
